b561aa9080d28abc84147148d70d25e7247adc7e
jacksonmoji <jacksonmoji@gmail.com>
2020-06-05 17:40:47 +0200

    Updates

---
+++ precache-manifest.457ca675c223cda1f6ed05cc5b6879d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5e4d3c5d84f684b47a7a8b2aef31fa",
+    "url": "/jacksonmoji/index.html"
+  },
+  {
+    "revision": "97010bb1263b9b95c8c3",
+    "url": "/jacksonmoji/static/css/main.6ab8b457.chunk.css"
+  },
+  {
+    "revision": "58f12c622fa0f0b3176e",
+    "url": "/jacksonmoji/static/js/2.dd13b002.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jacksonmoji/static/js/2.dd13b002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97010bb1263b9b95c8c3",
+    "url": "/jacksonmoji/static/js/main.e8207dde.chunk.js"
+  },
+  {
+    "revision": "a5f4b6daac8746172fbf",
+    "url": "/jacksonmoji/static/js/runtime-main.5cbfdc47.js"
+  },
+  {
+    "revision": "7ad01e4e6352e86da8a38bd9324e7f46",
+    "url": "/jacksonmoji/static/media/bg-7.7ad01e4e.png"
+  },
+  {
+    "revision": "8a85165567efefbdd42a3ad13e277118",
+    "url": "/jacksonmoji/static/media/profile_.8a851655.jpg"
+  }
+]);
